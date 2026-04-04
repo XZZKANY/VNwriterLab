7f7f701488a94968d5e1983c4b54d20bd4554286
预定 调和 <3241583594@qq.com>
2026-04-04 19:05:41 +0800

    feat: 实现预览引擎与路径推进基础

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { CharactersPage } from "./features/characters/pages/CharactersPage";
 import { EditorPage } from "./features/editor/pages/EditorPage";
 import { GraphPage } from "./features/graph/pages/GraphPage";
 import { LorePage } from "./features/lore/pages/LorePage";
+import { PreviewPage } from "./features/preview/pages/PreviewPage";
 import { ProjectHomePage } from "./features/projects/pages/ProjectHomePage";
 import "./styles.css";
 
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
       {
         path: "lore",
         element: <LorePage />,
+      },
+      {
+        path: "preview",
+        element: <PreviewPage />,
       },
     ],
   },
